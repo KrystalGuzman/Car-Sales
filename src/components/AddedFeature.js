803779264deb64161c4ec1085ec6718fc4ed4ab3
Krystal Guzman <kryguzman@gmail.com>
2020-02-18 08:17:03 -0800

    MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={()=>props.removeFeature(props.feature)} class="button is-dark">X</button>
       {props.feature.name}
     </li>
   );
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -8,11 +8,11 @@ const AdditionalFeatures = props => {
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} buyItem={props.buyItem} feature={item} />
           ))}
         </ol>
       ) : (
-        <p>Nice looking car!</p>
+        <p>Great looking car!</p>
       )}
     </div>
   );
